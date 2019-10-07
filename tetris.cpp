20dd6beeb2c538e9b524bb2e8a0208072319b413
ffy062 <56193560+ffy062@users.noreply.github.com>
2019-10-08 02:43:10 +0800

    Add files via upload

--- tetris.cpp
+++ tetris.cpp
@@ -7,13 +7,14 @@ class block;
 class game;
 
 class block{
+    friend game;
     private:
         bool error;
         int shape[4][5];
         int pos_x, pos_y;
         // string type;
     public:
-        block(string type = "\0", int pos_x = 0):pos_x(pos_x),pos_y(1) {
+        block(string type = "\0", int pos_x = 0):pos_x(pos_x-1),pos_y(1) {
             error = false;
             for(int i = 1; i < 4; ++i) {
                 shape[i][0] = -1;
@@ -197,19 +198,23 @@ class game {
     private:
     int row, col;
     char** hori;
-    int* cal;
+    int* cal, *height;
 
     public:
     game(int row = 0, int col = 0):row(row), col(col) {
+        int i;
+        
         hori = new char*[row];
-        for(int i = 0; i < row; ++i) {
+        for(i = 0; i < row; ++i) {
             hori[i] = new char[col];
             memset(hori[i], '0', col);
         }
         cal = new int[row];
-        for(int i = 0; i < row; ++i) {
+        for(i = 0; i < row; ++i) {
             cal[i] = 0;
         }
+        height = new int[col];
+        memset(height, row - 1, col);
     }
     void fall(string type, int pos) {
         block next(type, pos);
@@ -217,6 +222,27 @@ class game {
             cout << "Invalid block type\n";
             return;
         }
+        int touch = row - 1, tmp;
+
+        for(int i = 0; i < 4; ++i) {
+            if(next.shape[i][0] != -1) {
+                tmp = height[next.pos_x + i] - next.shape[i][0];
+                if(touch > tmp) {
+                    touch = tmp;
+                }
+            }
+        }
+        next.pos_y = touch;
+        for(int i = 0; i < 4; ++i) {
+            int j = 0;
+            while(next.shape[i][j] != -1) {
+                hori[next.pos_x + i][next.pos_y - next.shape[i][j]] = '1';
+                cal[next.pos_y - next.shape[i][j]]++;
+                height[next.pos_x + i] = next.pos_y - next.shape[i][j] - 1;
+                j++;
+            }
+        }
+
     }
     void show() {
         for(int i = 0; i < row; ++i) {
@@ -228,14 +254,20 @@ class game {
         }
     }
     void eliminate(int row_d) {
+        int i;
+        
         delete hori[row_d];
-        for(int i = row_d; i > 0; --i) {
+        for(i = row_d; i > 0; --i) {
             hori[i] = hori[i - 1];
             cal[i] = cal[i - 1];
         }
         hori[0] = new char[col];
         cal[0] = 0;
         memset(hori[0], '0', col);
+        for(i = 0; i < col; ++i) {
+            if(height[col] >= row_d)
+                height[col]++;
+        }
     }
     int check_full(int pos) {
         int val = -1;
@@ -250,6 +282,7 @@ class game {
 };
 
 int main() {
+    ios_base::sync_with_stdio(false);
     int row, col, pos;
     string type;
     
@@ -267,6 +300,7 @@ int main() {
             return 1;
         }
         tetris.fall(type, pos);
+        tetris.show();
     }
 
     return 0;
