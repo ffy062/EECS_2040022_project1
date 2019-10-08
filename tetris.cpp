9a3e3eb57085846501604d4b2ad523a04e8245e4
ffy062 <56193560+ffy062@users.noreply.github.com>
2019-10-09 02:52:23 +0800

    Add files via upload

--- tetris.cpp
+++ tetris.cpp
@@ -3,6 +3,8 @@
 #include <memory.h>
 using namespace std;
 
+#define debug
+
 class block;
 class game;
 
@@ -11,7 +13,7 @@ class block{
     private:
         bool error;
         int shape[4][5];
-        int pos_x, pos_y;
+        int pos_x, pos_y, len;
         // string type;
     public:
         block(string type = "\0", int pos_x = 0):pos_x(pos_x-1),pos_y(1) {
@@ -27,6 +29,7 @@ class block{
                 shape[1][2] = -1;
                 shape[2][0] = 1;
                 shape[2][1] = -1;
+                len = 3;
             }
             else if(type == "T2") {
                 shape[0][0] = 1;
@@ -35,6 +38,7 @@ class block{
                 shape[1][1] = 1;
                 shape[1][2] = 2;
                 shape[1][3] = -1;
+                len = 2;
             }
             else if(type == "T3") {
                 shape[0][0] = 0;
@@ -44,6 +48,7 @@ class block{
                 shape[1][2] = -1;
                 shape[2][0] = 0;
                 shape[2][1] = -1;
+                len = 3;
             }
             else if(type == "T4") {
                 shape[0][0] = 0;
@@ -52,6 +57,7 @@ class block{
                 shape[0][3] = -1;
                 shape[1][0] = 1;
                 shape[1][1] = -1;
+                len = 2;
             }
             else if(type == "L1") {
                 shape[0][0] = 0;
@@ -60,6 +66,7 @@ class block{
                 shape[0][3] = -1;
                 shape[1][0] = 0;
                 shape[1][1] = -1;
+                len = 2;
             }
             else if(type == "L2") {
                 shape[0][0] = 0;
@@ -69,6 +76,7 @@ class block{
                 shape[1][1] = -1;
                 shape[2][0] = 1;
                 shape[2][1] = -1;
+                len = 3;
             }
             else if(type == "L3") {
                 shape[0][0] = 2;
@@ -77,6 +85,7 @@ class block{
                 shape[1][1] = 1;
                 shape[1][2] = 2;
                 shape[1][3] = -1;
+                len = 2;
             }
             else if(type == "L4") {
                 shape[0][0] = 0;
@@ -86,6 +95,7 @@ class block{
                 shape[2][0] = 0;
                 shape[2][1] = 1;
                 shape[2][2] = -1;
+                len = 3;
             }
             else if(type == "S1") {
                 shape[0][0] = 0;
@@ -95,6 +105,7 @@ class block{
                 shape[1][2] = -1;
                 shape[2][0] = 1;
                 shape[2][1] = -1;
+                len = 3;
             }
             else if(type == "S2") {
                 shape[0][0] = 1;
@@ -103,6 +114,7 @@ class block{
                 shape[1][0] = 0;
                 shape[1][1] = 1;
                 shape[1][2] = -1;
+                len = 2;
             }
             else if(type == "Z1") {
                 shape[0][0] = 1;
@@ -112,6 +124,7 @@ class block{
                 shape[1][2] = -1;
                 shape[2][0] = 0;
                 shape[2][1] = -1;
+                len = 3;
             }
             else if(type == "Z2") {
                 shape[0][0] = 0;
@@ -120,6 +133,7 @@ class block{
                 shape[1][0] = 1;
                 shape[1][1] = 2;
                 shape[1][2] = -1;
+                len = 2;
             }
             else if(type == "J1") {
                 shape[0][0] = 0;
@@ -128,6 +142,7 @@ class block{
                 shape[1][1] = 1;
                 shape[1][2] = 2;
                 shape[1][3] = -1;
+                len = 2;
             }
             else if(type == "J2") {
                 shape[0][0] = 0;
@@ -139,6 +154,7 @@ class block{
                 shape[2][1] = -1;
                 shape[3][0] = 0;
                 shape[3][1] = -1;
+                len = 3;
             }
             else if(type == "J3") {
                 shape[0][0] = 0;
@@ -147,6 +163,7 @@ class block{
                 shape[0][3] = -1;
                 shape[1][0] = 2;
                 shape[1][1] = -1;
+                len = 2;
             }
             else if(type == "J4") {
                 shape[0][0] = 1;
@@ -156,6 +173,7 @@ class block{
                 shape[2][0] = 0;
                 shape[2][1] = 1;
                 shape[2][2] = -1;
+                len = 3;
             }
             else if(type == "I1") {
                 shape[0][0] = 0;
@@ -163,6 +181,7 @@ class block{
                 shape[0][2] = 2;
                 shape[0][3] = 3;
                 shape[0][4] = -1;
+                len = 1;
             }
             else if(type == "I2") {
                 shape[0][0] = 0;
@@ -173,6 +192,7 @@ class block{
                 shape[2][1] = -1;
                 shape[3][0] = 0;
                 shape[3][1] = -1;
+                len = 4;
             }
             else if(type == "O") {
                 shape[0][0] = 0;
@@ -181,6 +201,7 @@ class block{
                 shape[1][0] = 0;
                 shape[1][1] = 1;
                 shape[1][2] = -1;
+                len = 2;
             }
             else {
                 error = true;
@@ -190,8 +211,24 @@ class block{
         bool error_check() {
             return error;
         }
-
-
+        bool check_empty() {
+            for(int i = 0; i < 4; ++i) {
+                if(shape[i][0] != -1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        bool check_empty(int idx) {
+            return shape[idx][0] == -1 ? true : false;
+        }
+        void pop(int idx) {
+            for(int i = 0; i < 4; ++i) {
+                shape[idx][i] = shape[idx][i + 1];
+            }
+            shape[idx][4] = -1;
+            return;
+        }
 };
 
 class game {
@@ -207,28 +244,28 @@ class game {
         hori = new char*[row];
         for(i = 0; i < row; ++i) {
             hori[i] = new char[col];
-            memset(hori[i], '0', col);
+            memset(hori[i], '0', col * sizeof(char));
         }
         cal = new int[row];
         for(i = 0; i < row; ++i) {
             cal[i] = 0;
         }
         height = new int[col];
-        memset(height, row - 1, col);
-    }
-    void fall(string type, int pos) {
-        block next(type, pos);
-        if(next.error_check()) {
-            cout << "Invalid block type\n";
-            return;
+        for(i = 0; i < col; ++i) {
+            height[i] = row - 1;
         }
-        int touch = row - 1, tmp;
+    }
+    int fall(block& next, int pos) {
+        int touch =  row, tmp;
 
         for(int i = 0; i < 4; ++i) {
             if(next.shape[i][0] != -1) {
-                tmp = height[next.pos_x + i] - next.shape[i][0];
+                tmp = height[next.pos_x + i] + next.shape[i][0];
                 if(touch > tmp) {
                     touch = tmp;
+                    #ifdef debug
+                        cout << "change touch:" << touch << endl;
+                    #endif // debug
                 }
             }
         }
@@ -237,13 +274,15 @@ class game {
             int j = 0;
             while(next.shape[i][j] != -1) {
                 hori[next.pos_y - next.shape[i][j]][next.pos_x + i] = '1';
-                //cout << next.pos_x + i  << " " << next.pos_y - next.shape[i][j]  << " " << hori[next.pos_x + i][next.pos_y - next.shape[i][j]] << endl;
+                #ifdef debug
+                    cout << "new block " <<next.pos_y - next.shape[i][j]  << " " << next.pos_x + i << endl;
+                #endif // debug
                 cal[next.pos_y - next.shape[i][j]]++;
                 height[next.pos_x + i] = next.pos_y - next.shape[i][j] - 1;
                 j++;
             }
         }
-        return;
+        return next.pos_y;
 
     }
     void show() {
@@ -253,11 +292,24 @@ class game {
             }
             cout << endl;
         }
+        #ifdef debug
+            for(int i = 0; i < row; ++i) {
+                cout << cal[i] << " ";
+            }
+            cout << endl;
+            for(int i = 0; i < col; ++i) {
+                cout << height[i] << " ";
+            }
+            cout << endl;
+        #endif // debug
     }
     void eliminate(int row_d) {
         int i;
-
+        #ifdef debug
+            cout << "delete row" << row_d << endl;
+        #endif // debug
         delete hori[row_d];
+
         for(i = row_d; i > 0; --i) {
             hori[i] = hori[i - 1];
             cal[i] = cal[i - 1];
@@ -266,43 +318,67 @@ class game {
         cal[0] = 0;
         memset(hori[0], '0', col);
         for(i = 0; i < col; ++i) {
-            if(height[col] >= row_d)
-                height[col]++;
+            if(height[i] <= row_d) {
+                height[i]++;
+            }
+
         }
     }
-    int check_full(int pos) {
-        int val = -1;
-        for(int i = 0; i < 4; ++i) {
-            if(cal[pos - i] == col) {
-                eliminate(pos - i);
-                val = i;
+    void check_full(int pos) {
+        #ifdef debug
+            cout << "check full start position: " << pos << endl;
+        #endif // debug
+        for(int i = 0, j = 0; i < 4; ++i, ++j) {
+            #ifdef debug
+                cout << "position: " << pos - j << " number: " << cal[pos - j] << endl;
+            #endif // debug
+            if(cal[pos - j] == col) {
+                eliminate(pos - j);
+                --j;
             }
         }
-        return val;
+        return;
     }
 };
 
 int main() {
     ios_base::sync_with_stdio(false);
-    int row, col, pos;
+    int row, col, pos, stop;
     string type;
+    ifstream myfile;
+    myfile.open("tetris.data.txt");
 
-    cin >> row >> col;
+    myfile >> row >> col;
     if(row > 40 || col > 15 || row < 1 || col < 1) {
         cout << "Invalid value of row or cloumn\n";
         return 1;
     }
 
     game tetris(row, col);
+    #ifdef debug
+        tetris.show();
+    #endif // debug
 
-    while(cin >> type && type != "End") {
-        if(!(cin >> pos) || pos < 1) {
+    while(myfile >> type && type != "End") {
+        if(!(myfile >> pos) || pos < 1) {
             cout << "Invalid operation\n";
             return 1;
         }
-        tetris.fall(type, pos);
-        tetris.show();
+        #ifdef debug
+            cout << type << " " << pos << endl;
+        #endif // debug
+        block next(type, pos);
+        if(next.error_check()) {
+            cout << "Invalid block type\n";
+            return 1;
+        }
+        stop = tetris.fall(next, pos);
+        tetris.check_full(stop);
+        #ifdef debug
+            tetris.show();
+        #endif // debug
     }
+    myfile.close();
 
     return 0;
 }
